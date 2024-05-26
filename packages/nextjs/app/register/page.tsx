87e82c3db012d5e3c497ed295f222d6c07ba3664
paul.sizon <paul.sizon@outlook.com>
2024-05-26 15:11:54 -0300

    flow for disconnected wallet

--- packages/nextjs/app/register/page.tsx
+++ packages/nextjs/app/register/page.tsx
@@ -5,12 +5,15 @@ import { useAccount } from 'wagmi';
 import { useRouter } from 'next/navigation'
 
 import { supabase } from '~~/utils/supabase/client';
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 
 const ProfilePage = () => {
   const router = useRouter();
   const [file, setFile] = useState("");
   const [cid, setCid] = useState("");
   const [uploading, setUploading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   const inputFile = useRef(null);
   const [mode, setMode] = useState("register");  // 'register' or 'update'
 
@@ -26,6 +29,9 @@ const ProfilePage = () => {
   useEffect(() => {
     if (address) {
       loadProfile(address);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);      
     }
   }, [address]);
 
@@ -97,8 +103,11 @@ const ProfilePage = () => {
 
 
   const loadProfile = async (walletAddress) => {
+    setIsLoading(true);
+
     if (!walletAddress) {
       console.log("No wallet address provided.");
+      setIsLoading(false);
       return;
     }
 
@@ -111,6 +120,7 @@ const ProfilePage = () => {
     if (error) {
       console.error('Error loading profile:', error);
       alert('Could not load profile data.');
+      setIsLoading(false);
     } else if (data) {
       setProfile({
         name: data.name,
@@ -118,123 +128,97 @@ const ProfilePage = () => {
         creatorWalletAddress: data.wallet,
         image: data.avatar_url || `https://robohash.org/${walletAddress}.png?set=set5`
       });
-      setMode("update");  // Set mode to update since profile data was found
+      setMode("update");
+      setIsLoading(false);
     } else {
       console.log(`No profile found for wallet: ${walletAddress}`);
-      setMode("register");  // Set mode to register as no profile was found
+      setMode("register");
+      setIsLoading(false);
     }
   };
 
 
-
-  const updateProfile = async () => {
-    try {
-      const profileData = {
-        name: profile.name,
-        description: profile.description,
-        avatar_url: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}`  // Ensure cid is defined
-      };
-
-      const { data, error } = await supabase
-        .from('profiles')
-        .update(profileData)
-        .eq('wallet', address);  // 'address' is the wallet address of the profile to update
-
-      if (error) {
-        console.error('Error updating profile:', error);
-        alert(`Could not update profile: ${error.message}`);
-      } else {
-        alert('Profile updated successfully!');
-      }
-    } catch (e) {
-      console.error('Unexpected error:', e);
-      alert('An unexpected error occurred');
-    }
-  };
-
-  const deleteProfile = async (walletAddress) => {
-    try {
-      const { data, error } = await supabase
-        .from('profiles')
-        .delete()
-        .eq('wallet', walletAddress);  // Specify the wallet address of the profile to delete
-
-      if (error) {
-        console.error('Error deleting profile:', error);
-        alert(`Could not delete profile: ${error.message}`);
-      } else {
-        alert('Profile deleted successfully!');
-      }
-    } catch (e) {
-      console.error('Unexpected error:', e);
-      alert('An unexpected error occurred');
-    }
-  };
-
-
-
-  return (
-    <div className="max-w-md mx-auto mt-10 p-4 bg-white shadow-md rounded-md">
-      <h2 className="text-2xl font-semibold mb-4">{mode === "update" ? "Update Your Profile" : "Register Your Profile"}</h2>
-      <div className="mb-4">
-        <label htmlFor="file" className="block text-sm font-medium text-gray-700 mb-1">Upload Avatar</label>
-        <input type="file" id="file" ref={inputFile} onChange={handleChange} style={{ display: 'none' }} />
-        <button
-          className={`w-full text-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${uploading ? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-700'}`}
-          disabled={uploading}
-          onClick={() => inputFile.current.click()}
-        >
-          {uploading ? "Uploading..." : "Upload Image"}
-        </button>
-        {cid && (
-          <div className="mt-2 text-center">
-            <img
-              src={`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}`}
-              alt="Profile Avatar"
-              className="mx-auto h-20 w-20 rounded-full"
-            />
+  return (    
+      <div className="relative max-w-md mx-auto mt-10">
+        {isLoading && (
+          <div className="absolute inset-0 bg-gray-100 bg-opacity-75 flex justify-center items-center">
+            <span className="loading loading-ring loading-lg"></span>
           </div>
         )}
+        <div className={`p-4 bg-white shadow-md rounded-md ${isLoading ? 'opacity-50' : 'opacity-100'}`}>
+          {address ? (
+            <>
+              <h2 className="text-2xl font-semibold mb-4">{mode === "update" ? "Update Your Profile" : "Register Your Profile"}</h2>
+              <div className="mb-4">
+                <label htmlFor="file" className="block text-sm font-medium text-gray-700 mb-1">Upload Avatar</label>
+                <input type="file" id="file" ref={inputFile} onChange={handleChange} style={{ display: 'none' }} />
+                <button
+                  className={`w-full text-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${uploading ? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-700'}`}
+                  disabled={uploading}
+                  onClick={() => inputFile.current.click()}
+                >
+                  {uploading ? "Uploading..." : "Upload Image"}
+                </button>
+                {cid && (
+                  <div className="mt-2 text-center">
+                    <img
+                      src={`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}`}
+                      alt="Profile Avatar"
+                      className="mx-auto h-20 w-20 rounded-full"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={profile.name || ''}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Description</label>
+                <textarea
+                  name="description"
+                  value={profile.description || ''}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  rows="3"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Wallet Address</label>
+                <input
+                  type="text"
+                  name="creatorWalletAddress"
+                  value={profile.creatorWalletAddress || address || ''}
+                  readOnly
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm text-gray-500 bg-gray-100 sm:text-sm"
+                />
+              </div>
+              <button
+                className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+                onClick={saveProfile}
+              >
+                {mode === "update" ? "Update Profile" : "Create Profile"}
+              </button>
+            </>
+          ) : (
+            <div className="text-center p-4">
+              <h2 className="text-xl font-semibold">Wallet Not Connected</h2>
+              <p className="text-gray-600">Please connect your wallet to access the profile page.</p>
+              <RainbowKitCustomConnectButton />
+            </div>
+          )}
+        </div>
       </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-700">Name</label>
-        <input
-          type="text"
-          name="name"
-          value={profile.name || ''}
-          onChange={handleInputChange}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-700">Description</label>
-        <textarea
-          name="description"
-          value={profile.description || ''}
-          onChange={handleInputChange}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          rows="3"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block text-sm font-medium text-gray-700">Wallet Address</label>
-        <input
-          type="text"
-          name="creatorWalletAddress"
-          value={profile.creatorWalletAddress || address || ''}
-          readOnly
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm text-gray-500 bg-gray-100 sm:text-sm"
-        />
-      </div>
-      <button
-        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
-        onClick={saveProfile}
-      >
-        {mode === "update" ? "Update Profile" : "Create Profile"}
-      </button>
-    </div>
-  );
-};
+    );
+  };
+    
+
 
 export default ProfilePage;
 
